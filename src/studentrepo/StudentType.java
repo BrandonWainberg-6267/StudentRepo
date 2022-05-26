8a26d39e6c44f0a4bf3a43feea4fe2be2a8779f8
Brandon Wainberg <wainberg@sheridancollege.ca>
2022-05-26 13:56:42 -0400

    Created a new class StudentType

---
+++ src/studentrepo/StudentType.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentrepo;
+
+/**
+ *
+ * @author bmw26
+ */
+public class StudentType {
+    private String type;
+    private String id;
+    StudentType(String stdType, String stdId)
+    {
+        type=stdType;
+        id=stdId;
+                
+    }
+}
